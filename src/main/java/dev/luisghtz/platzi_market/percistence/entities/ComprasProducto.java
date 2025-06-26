cc2581fe68bb00db37c1487e95938f8e30c2c50e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-26 13:21:31 -0600

    Add @MapsId

--- src/main/java/dev/luisghtz/platzi_market/percistence/entities/ComprasProducto.java
+++ src/main/java/dev/luisghtz/platzi_market/percistence/entities/ComprasProducto.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -23,6 +24,7 @@ public class ComprasProducto {
   private Boolean estado;
 
   @ManyToOne
+  @MapsId("idCompra")
   @JoinColumn(name = "id_compra", insertable = false, updatable = false)
   private Compra compra;
 
