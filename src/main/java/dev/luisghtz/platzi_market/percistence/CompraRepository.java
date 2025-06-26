ec01e4b65c99f0a625d73a7ea25a72a387c476dd
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-26 13:38:37 -0600

    Add missing annotation @Repository

--- src/main/java/dev/luisghtz/platzi_market/percistence/CompraRepository.java
+++ src/main/java/dev/luisghtz/platzi_market/percistence/CompraRepository.java
@@ -3,6 +3,8 @@ package dev.luisghtz.platzi_market.percistence;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import dev.luisghtz.platzi_market.domain.Purchase;
 import dev.luisghtz.platzi_market.domain.repositories.PurchaseRepository;
 import dev.luisghtz.platzi_market.percistence.crud.CompraCrudRepository;
@@ -10,6 +12,7 @@ import dev.luisghtz.platzi_market.percistence.entities.Compra;
 import dev.luisghtz.platzi_market.percistence.mapper.PurchaseMapper;
 import lombok.RequiredArgsConstructor;
 
+@Repository
 @RequiredArgsConstructor
 public class CompraRepository implements PurchaseRepository {
   private final CompraCrudRepository compraCrudRepository;
