65da23b2521592673936b7c0430d58fc7bce21a9
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-26 11:12:23 -0600

    Create PurchateRepository

---
+++ src/main/java/dev/luisghtz/platzi_market/domain/repositories/PurchateRepository.java
@@ -0,0 +1,14 @@
+package dev.luisghtz.platzi_market.domain.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import dev.luisghtz.platzi_market.domain.Purchase;
+
+public interface PurchateRepository {
+  List<Purchase> getAll();
+
+  Optional<List<Purchase>> getByClientId(String clientId);
+
+  Purchase save(Purchase purchase);
+}
